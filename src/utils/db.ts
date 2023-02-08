be79347bdff2e7fda25ff0f5adeffb206dd5bdc6
Vadym Baranov <vadym.baranov@hotmail.com>
2023-02-08 19:01:55 +0200

    Build fixes

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,28 +1,28 @@
-/* eslint-disable max-len */
-import * as dotenv from 'dotenv';
-import * as pg from 'pg';
-import { Sequelize } from 'sequelize';
-// import { createClient } from '@supabase/supabase-js';
+// /* eslint-disable max-len */
+// import * as dotenv from 'dotenv';
+// import * as pg from 'pg';
+// import { Sequelize } from 'sequelize';
+// // import { createClient } from '@supabase/supabase-js';
 
-dotenv.config();
+// dotenv.config();
 
-const { SUPABASE_HOST, SUPABASE_PASSWORD } = process.env;
+// const { SUPABASE_HOST, SUPABASE_PASSWORD } = process.env;
 
-// const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+// // const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// export default supabase;
+// // export default supabase;
 
-const dbName: string = process.env.DB_NAME || '';
-const dbUsername: string = process.env.DB_USERNAME || '';
+// const dbName: string = process.env.DB_NAME || '';
+// const dbUsername: string = process.env.DB_USERNAME || '';
 
-// // export const sequelize = new Sequelize(URI);
+// // // export const sequelize = new Sequelize(URI);
 
-if (!SUPABASE_HOST || !SUPABASE_PASSWORD) {
-  throw new Error('Missing Database config');
-}
+// if (!SUPABASE_HOST || !SUPABASE_PASSWORD) {
+//   throw new Error('Missing Database config');
+// }
 
-export const sequelize = new Sequelize(dbUsername, dbName, SUPABASE_PASSWORD, {
-  host: SUPABASE_HOST,
-  dialect: 'postgres',
-  dialectModule: pg,
-});
+// export const sequelize = new Sequelize(dbUsername, dbName, SUPABASE_PASSWORD, {
+//   host: SUPABASE_HOST,
+//   dialect: 'postgres',
+//   dialectModule: pg,
+// });
